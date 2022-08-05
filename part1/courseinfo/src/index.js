c367d91523bfd82a8311470ea282503e7ad2ca22
Yesid Rincon <dairo.rincon@uptc.edu.co>
2022-08-05 12:22:59 -0500

    Exercise 1.2 solved

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course} </h1>
+      
+    </div>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.content.part1} exercises={props.content.exercises1}/>
+      <Part part={props.content.part2} exercises={props.content.exercises2}/>
+      <Part part={props.content.part3} exercises={props.content.exercises3}/>
+      
+    </div>
+  )
+}
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const content = { part1, exercises1, part2, exercises2, part3, exercises3 }
+  return (
+    <div>
+      <Header course={course}/>      
+      <Content content={content} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
