347bc6f52a1938dbd5f65fd41e893de0ec67af54
Yesid Rincon <dairo.rincon@uptc.edu.co>
2022-08-06 21:34:49 -0500

    Exercises 1.6 -1.11 solved

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,53 +1,95 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Statistic = ({text, value}) => {
+  if (text === 'positive') {
+    return (
+      <tr>
+        <td>{text} </td>
+        <td>{value} %</td>
+      </tr>
+    )
+  }
+
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>
-        So you were born in {bornYear()}
-        </p>   
+    <tr>
+      <td>{text} </td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({clicks}) => {
+
+    const {good, neutral, bad} = clicks
+    const total = good + neutral + bad
+    const average = (clicks.good * 1 + clicks.bad * -1) / total
+    const positive = clicks.good * (100/total)
+
+    if (total !== 0) {
+      return (
+        <div>
+          <table>
+            <tbody>
+              <Statistic text="good" value={good} />
+              <Statistic text="neutral" value={neutral} />
+              <Statistic text="bad" value={bad} />
+              <Statistic text="all" value={total} />
+              <Statistic text="average" value={average} />
+              <Statistic text="positive" value={positive} />
+            </tbody>
+          </table>
+        </div>
+      )
+    }
+    return (
+      <div>
+        No feedback given
       </div>
-  )
-}
-const Display = ({counter}) => {
+    )
 
-  return (
-    <div>{counter}</div>
-  )
+    
 }
 
-const Button = ({handleClick, text}) => {
-  
-  return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-}
+
+
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  // save clicks of each button to own state
+  const [clicks, setClicks] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const handleGoodClick = () =>
+    setClicks({...clicks, good: clicks.good + 1})
+
+  const handleNeutralClick = () =>
+    setClicks({...clicks, neutral: clicks.neutral + 1})
+
+  const handleBadClick = () =>
+    setClicks({...clicks, bad: clicks.bad + 1})
+  console.log(`clicks for good is --> ${clicks.good}`)
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button handleClick={increaseByOne} text='plus'/>
-      <Button handleClick={setToZero} text='zero'/>
-      <Button handleClick={decreaseByOne} text='minus'/>
+      
+      <h1>Customer feedback</h1>
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <h1>Statistics</h1>
+      <Statistics clicks={clicks} />
     </div>
   )
 }
 
-ReactDOM.render(
-  <App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
